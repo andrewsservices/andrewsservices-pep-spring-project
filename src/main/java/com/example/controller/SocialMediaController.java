d6b857b044b6ca4a36e72fee62b33546f1a44e51
Andrew Lachin <admin@andrewlachin.dev>
2024-11-06 16:10:08 +0000

    First Commit

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import java.util.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.entity.Message;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +16,19 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@Controller
+@RequestMapping("messages")
 public class SocialMediaController {
+
+    private MessageService messageService;
+
+    public SocialMediaController(MessageService messageService){
+        this.messageService = messageService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody List<Message> getMessagesList(){
+        return messageService.getMessages();
+    }
 
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,27 @@
 package com.example.service;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.exception.ResourceNotFoundException;
+
+@Service
 public class MessageService {
+    
+
+
+    @Autowired
+    public MessageService(){};
+
+
+
+    public List<Message> getMessages(){
+        List<Message> messages = new ArrayList<>();
+        return messages;
+    }
+
+    
 }
